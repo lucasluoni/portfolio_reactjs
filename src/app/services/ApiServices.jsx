4c857279a6bfa23c3213ed4c868274d2cc38d275
lucasluoni <lucasluoni@gmail.com>
2022-01-08 17:12:37 -0300

    apaga codigo comentado em ApiService

--- src/app/services/ApiServices.jsx
+++ src/app/services/ApiServices.jsx
@@ -1,7 +1,6 @@
 // apiService para regras de negócio do back end
 import { get } from './httpService'
 
-// const BACK_END_URL = 'http://localhost:8888/lucas_luoni/wp-json/wp/v2'
 const BACK_END_URL =
   process.env.NODE_ENV === 'development' ?
     'http://localhost:8888/lucas_luoni/wp-json/wp/v2' :
@@ -11,7 +10,6 @@ export async function apiGetWorksData() {
   const JsonData = await get(`${BACK_END_URL}/trabalhos`)
   return JsonData
 }
-// console.log('process.env.NODE_ENV =', process.env.NODE_ENV)
 
 export async function apiGetAboutData() {
   const AboutData = await get(`${BACK_END_URL}/pages/28`)
